a54740121e96cddc22c1feee6146bc4b160643c8
CawanCarv <cawancarvalho3@gmail.com>
2023-06-06 15:56:24 -0300

    musicalweek v0.3

--- src/app/(salas)/sala/[id]/page.js
+++ src/app/(salas)/sala/[id]/page.js
@@ -4,7 +4,7 @@ import Loading from '@/app/loading.js';
 
 export default function Sala({ params }) {
   return (
-    <main className='mx-auto relative -z-10 sm:max-w-7xl px-2 sm:px-6 lg:px-8 min-h-[80vh] flex justify-center items-center'>
+    <main className='mx-auto relative -z-10 sm:max-w-7xl px-2 sm:px-6 lg:px-8 min-h-[80vh] flex justify-center items-start'>
       <Suspense fallback={<Loading />}>
         <ProcuraMusicaSala id_musica_sala={params.id} />
       </Suspense>
--- src/components/Sala/Sala.js
+++ src/components/Sala/Sala.js
@@ -23,7 +23,7 @@ const Sala = async ({ sala, id_usuario }) => {
   return (
     <>
       <Contador className="text-black dark:text-white sm:text-3xl absolute top-4 right-[42%] sm:right-4" targetDate={sala.tempo_restante}/>
-      <div className="grid mt-10 sm:mt-0 grid-cols-1 gap-5 min-h-[50vh] min-w-full mb-10 relative">
+      <div className="grid mt-10 sm:mt-20 grid-cols-1 gap-5 min-h-[50vh] min-w-full mb-10 relative">
         <Suspense fallback={<Loading />}>
           <MusicaSala
             key={sala.musicas[musicKey].id_musica_sala}
