7605a89f49cf65d8541bf570f97431fbfaff1db3
GabrielMeira <gabegm03@outlook.com>
2023-11-05 02:10:40 -0300

    Finalização - Troca de ícones no perfil

--- src/components/perfil/Modal.js
+++ src/components/perfil/Modal.js
@@ -1,20 +1,14 @@
 "use client"
 import { useState, useEffect } from 'react';
-import Image from "next/image";
+import Image from 'next/image';
 import { useCookies } from 'react-cookie';
 import Modal from 'react-modal';
-
-import perfil from './profilePics/perfilzada.png';
-import bolsobreck from './profilePics/bolsobreck.jpg';
-import indioEmo from './profilePics/emo_indio_bombado.jpg';
-import bomDia from './profilePics/bomdiamor.png';
-import shock from './profilePics/static_shock.jpg';
 import { alterarPerfil } from '@/utils/user';
 
 
 async function handleClick(data) {
     const propertyNames = Object.values(data);
-    console.log("URLL:" + propertyNames);
+    //console.log("URLL:" + propertyNames);
 
     const res = await alterarPerfil(data);
     console.log(res);
@@ -31,18 +25,20 @@ export default function ModalProfile(props) {
     const [cookies, setCookie] = useCookies(['token']);
     const [showConfirmationModal, setShowConfirmationModal] = useState(false);
     const [profilePics, setProfilePics] = useState([
-        perfil,
-        bolsobreck,
-        indioEmo,
-        bomDia,
-        shock]);
+        0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29
+    ]);
+    
+
+    //console.log(profilePics);
 
     //console.log(props.perfil);
     return (
         <div>
             <button
                 className={`bg-teal-500 hover:bg-teal-600 text-white rounded-lg px-4 py-2`}
-                onClick={() => setShowConfirmationModal(true)}
+                onClick={() => {
+                                    setShowConfirmationModal(true);
+                                }}
             >
                 Alterar Foto de Perfil
             </button>
@@ -57,42 +53,39 @@ export default function ModalProfile(props) {
             >
                 <div className="bg-zinc-950 p-6 border border-gray-600 rounded-lg shadow-lg w-4/5 mx-auto flex flex-col items-center justify-center">
                     <h2 className="text-2xl font-semibold mb-6 text-black">Selecione a foto desejada</h2>
-                    <div className='grid grid-cols-5 gap-7 mb-6 mx-5'>
-                        {profilePics.map((imageInfo, index) => (                            
-                            <div key={index} className="relative">
-                                <button
-                                    onClick={() => {
-                                        console.log("BANCO:" + Object.values(props.perfil))
-                                        const url = Object.values(imageInfo);
-                                        console.log("CLIQUE:" + url);
+                    <div className='grid grid-cols-10 gap-3 mb-6 mx-5'>
+                        {
+                            profilePics.map((imageInfo, index) => (                            
+                                <div key={index} className="relative">
+                                    <button
+                                        onClick={() => {
+                                            var d = new Date(props.perfil.data_nasc);
+                                            var date = [
+                                                d.getFullYear(),
+                                                ('0' + d.getDate()).slice(-2),
+                                                ('0' + (d.getMonth() + 1)).slice(-2)
+                                                
+                                            ].join('-');                                        
 
-                                        var d = new Date(props.perfil.data_nasc);
-                                        var date = [
-                                            d.getFullYear(),
-                                            ('0' + (d.getMonth() + 1)).slice(-2),
-                                            ('0' + d.getDate()).slice(-2)
-                                        ].join('-');
-                                        //var date = Date.parse(props.perfil.data_nasc); 
+                                            const data = {
+                                                nome: props.perfil.nome,
+                                                nick: props.perfil.nick,
+                                                data_nasc: date,
+                                                icon: "icone" + imageInfo + ".png"
+                                            };
+                                            handleClick(data);
+                                        }}
+                                    >
+                                        <Image
+                                            src={'/icones/icone' + imageInfo + ".png"}
+                                            alt={imageInfo}
 
-                                        const data = {
-                                            nome: props.perfil.nome,
-                                            nick: props.perfil.nick,
-                                            data_nasc: date,
-                                            icon: imageInfo.src
-                                        };
-                                        handleClick(data);
-                                    }}
-                                >
-                                    <Image
-                                        src={imageInfo}
-                                        alt={index}
-
-                                        width="400"
-                                        height="0"
-                                        className="rounded hover:opacity-40"
-                                    />
-                                </button>
-                            </div>
+                                            width="125"
+                                            height="0"
+                                            className="rounded hover:opacity-40 border-solid border-2 mb-1 border-zinc-800 "
+                                        />
+                                    </button>
+                                </div>
                         ))}
                         
                     </div>
--- src/middleware.js
+++ src/middleware.js
@@ -34,6 +34,6 @@ export function middleware(request) {
 
 export const config = {
   matcher: [
-    '/((?!_next).*)',
+    '/((?!api|icones|images|_next/static|_next/image|favicon.ico).*)',
   ],
 }
