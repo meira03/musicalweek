f2c05adfa229b4bc6248ce2a3d26ed0f890c56e1
Cawan Carvalho <cawancarvalho3@gmail.com>
2023-11-06 20:15:08 -0300

    update next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'i.scdn.co',
-            port: '',
-            pathname: './**',
-          },
-        ],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'i.scdn.co',
+          port: '',
+          pathname: './**',
+        },
+      ],
     }
 }
 
