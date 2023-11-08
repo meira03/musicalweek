ae49ea4c902cfdb789e7e66ff75be39e41497389
GabrielMeira <gabegm03@outlook.com>
2023-11-08 01:19:15 -0300

    Atualização da função de sair da sala do artista.

--- src/components/sala/BtnSalaArtista.js
+++ src/components/sala/BtnSalaArtista.js
@@ -1,5 +1,5 @@
 "use client";
-import { entraSalaArtista } from "@/utils/artista";
+import { entraSalaArtista, sairSalaArtista } from "@/utils/artista";
 
 export const BtnSalaArtista = ({ participante, id_sala }) => {
 
@@ -8,8 +8,9 @@ export const BtnSalaArtista = ({ participante, id_sala }) => {
         res ? window.location.reload : console.log("Erro interno no servidor");
     }
 
-    function sairSala() {
-        console.log("código Meira")
+    function sairSala(id_musica_sala) {
+      console.log("código Meira");
+      sairSalaArtista(id_musica_sala);
     }
 
   return (
@@ -19,7 +20,7 @@ export const BtnSalaArtista = ({ participante, id_sala }) => {
           Participar da Sala
         </button>
       ) : (
-        <button onClick={sairSala} className="px-3 py-2 bg-red-600 white-text">
+        <button onClick={sairSala(id_sala)} className="px-3 py-2 bg-red-600 white-text">
           Sair da Sala
         </button>
       )}
--- src/utils/artista.js
+++ src/utils/artista.js
@@ -63,3 +63,24 @@ export async function entraSalaArtista(id_sala) {
       return false;
     })
 }
+
+export async function sairSalaArtista(id_sala_artista) {
+  const cookieStore = cookies();
+  
+  const url =
+    `https://musicalweek-api.azurewebsites.net/endpoints/sala/artista/index.php?id_sala=${id_sala_artista}`;
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append("Authorization", "Bearer " + cookieStore.get("token").value);
+
+  return await fetch(url, {
+    method: "DELETE",
+    headers: headers,
+    credentials: "include",
+    cache: "no-store",
+  })
+    .then((result) => result.json())
+    .then((res) => {
+      return res
+    });
+}
