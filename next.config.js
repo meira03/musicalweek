074286e4a0f95559059683bde001bf7f56871bff
Cawan Carvalho <cawancarvalho3@gmail.com>
2023-11-09 01:31:57 -0300

    security headers

--- next.config.js
+++ next.config.js
@@ -10,6 +10,35 @@ const nextConfig = {
         },
       ],
     }
+    ,async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Strict-Transport-Security',
+              value: 'max-age=31536000; includeSubDomains; preload',
+            },
+            {
+              key: 'X-Frame-Options',
+              value: 'DENY',
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+            {
+              key: 'Referrer-Policy',
+              value: 'strict-origin-when-cross-origin',
+            },
+            {
+              key: 'Permissions-Policy',
+              value: 'geolocation=(self "https://example.com"); microphone=()',
+            },
+          ],
+        },
+      ];
+    },
 }
 
 module.exports = nextConfig
