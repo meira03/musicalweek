39f274c2b1b383630b56d6850aae4930945205a4
felipekenzo175 <felipekenzoknz@gmail.com>
2023-11-11 20:07:21 -0300

    atualização do design da confirmação de email

--- src/app/[lang]/confirmaEmail/page.js
+++ src/app/[lang]/confirmaEmail/page.js
@@ -3,18 +3,21 @@ import React from 'react';
 
 const EmailConfirmed = () => {
   const redirectToHome = () => {
-    window.location.href = '/home';
+    window.location.href = '/';
   };
 
   return (
     <div className="min-h-screen flex flex-col justify-center items-center text-white">
       <div className="container mx-auto p-8 text-center">
-        <h1 className="text-4xl font-semibold mb-6">Email Confirmado com Sucesso!</h1>
-        <p className="text-lg font-semibold mb-8">
-          Seu email foi confirmado com sucesso! Você agora pode aproveitar os recursos da nossa aplicação.
+        <h1 className="flex justify-center neon-text text-4xl uppercase font-semibold mb-32">Email Confirmado com Sucesso!</h1>
+        <p className="text-2xl font-semibold mb-8 text-white">
+          Seu email foi confirmado com sucesso!
+        </p>
+        <p className="text-2xl font-semibold mb-32 text-white">
+          Você agora pode aproveitar os recursos da nossa aplicação.
         </p>
         <button
-          className="bg-teal-500 hover-bg-teal-600 text-white font-xbold py-3 px-20 rounded-lg text-xl"
+          className="bg-black-100 dark:bg-black-300 border-2 border-neon-blue-300 text-white hover:text-gray-400 font-bold py-2 px-4 focus:outline-none focus:shadow-outline w-full font-family:tech"
           onClick={redirectToHome}
         >
           Ir para a página inicial
