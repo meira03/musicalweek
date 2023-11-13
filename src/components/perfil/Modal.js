02805e3797e6e46db3fc927240790554acc0e7c1
GabrielMeira <62723704+meira03@users.noreply.github.com>
2023-11-12 22:45:08 -0300

    Update Modal.js - centralização do modal e correção da seleção de imagens

--- src/components/perfil/Modal.js
+++ src/components/perfil/Modal.js
@@ -6,19 +6,6 @@ import Modal from 'react-modal';
 import { alterarPerfil } from '@/utils/user';
 
 
-async function handleClick(data) {
-    const propertyNames = Object.values(data);
-    //console.log("URLL:" + propertyNames);
-
-    const res = await alterarPerfil(data);
-    console.log(res);
-        if (res.sucesso === true){ 
-            window.location.href = '/perfil'
-            setShowConfirmationModal(false);
-        }
-        //else document.getElementById('search-error').innerHTML = res.error
-}
-
 Modal.setAppElement(null);
 
 export default function ModalProfile(props) {
@@ -31,6 +18,19 @@ export default function ModalProfile(props) {
 
     function onHover(){
         setImageHover(true);
+    }
+
+    async function handleClick(data) {
+        const propertyNames = Object.values(data);
+        //console.log("URLL:" + propertyNames);
+    
+        const res = await alterarPerfil(data);
+        console.log(res);
+            if (res.sucesso === true){ 
+                window.location.href = '/perfil'
+                setShowConfirmationModal(false);
+            }
+            //else document.getElementById('search-error').innerHTML = res.error
     }
 
     //console.log(profilePics);
@@ -54,7 +54,7 @@ export default function ModalProfile(props) {
                     height={0}
                     width={0}
                     sizes="100vw"
-                    className="object-cover rounded-full hover:opacity-50 w-full h-64 sm:h-80 xl:h-96 "
+                    className="object-cover rounded-full hover:opacity-50 w-auto h-64 sm:h-80 xl:h-48 "
                     onMouseEnter={() => {
                         setImageHover(true);
                     }}
@@ -70,58 +70,61 @@ export default function ModalProfile(props) {
                 onRequestClose={() => setShowConfirmationModal(false)}
                 contentLabel="Escolha sua foto de perfil"
                 ariaHideApp={false}
-                className="modal static inset-0 flex items-center justify-center z-50 "
-                overlayClassName="modal-overlay fixed inset-0 bg-black overflow-auto"
+                className="modal static inset-0 flex items-center justify-center z-50 m-auto "
+                overlayClassName="modal-overlay absolute inset-0 bg-black overflow-auto"
             >
-                <div className="bg-zinc-950 p-6 border border-gray-600 shadow-lg w-4/5 mx-auto flex flex-col items-center justify-center align-middle">
-                    <h2 className="text-2xl font-semibold mb-6 text-black">Selecione a foto desejada</h2>
-                    <div className='grid 
-                                        grid-cols-2 gap-3
-                                        sm:grid-cols-3 sm:gap-3
-                                        lg:grid-cols-6 lg:gap-3 lg:mb-6 lg:mx-5
-                                        xl:grid-cols-10 xl:gap-3 xl:mb-6 xl:mx-5
-                                    '>
-                        {
-                            profilePics.map((imageInfo, index) => (                            
-                                <div key={index} className="relative">
-                                    <button
-                                        onClick={() => {
-                                            var d = new Date(props.perfil.data_nasc);
-                                            var date = [
-                                                d.getFullYear(),
-                                                ('0' + d.getDate()).slice(-2),
-                                                ('0' + (d.getMonth() + 1)).slice(-2)
-                                                
-                                            ].join('-');                                        
+                <div className='absolute top-0 left-0 h-screen w-screen flex justify-center items-center'>
+                    <div className="bg-zinc-950 p-6 border border-gray-600 shadow-lg w-4/5 mx-auto flex flex-col items-center justify-center align-middle">
+                        <h2 className="text-2xl font-semibold mb-6 text-black">Selecione a foto desejada</h2>
+                        <div className='grid 
+                                            grid-cols-2 gap-3
+                                            sm:grid-cols-3 sm:gap-3
+                                            lg:grid-cols-6 lg:gap-3 lg:mb-6 lg:mx-5
+                                            xl:grid-cols-10 xl:gap-3 xl:mb-6 xl:mx-5
+                                        '>
+                            {
+                                profilePics.map((imageInfo, index) => (                            
+                                    <div key={index} className="relative">
+                                        <button
+                                            onClick={() => {
+                                                var d = new Date(props.perfil.data_nasc);
+                                                var date = [
+                                                    d.getFullYear(),
+                                                    ('0' + d.getDate()).slice(-2),
+                                                    ('0' + (d.getMonth() + 1)).slice(-2)
+                                                    
+                                                ].join('-');                                        
 
-                                            const data = {
-                                                nome: props.perfil.nome,
-                                                nick: props.perfil.nick,
-                                                data_nasc: date,
-                                                icon: "icone" + imageInfo + ".png"
-                                            };
-                                            handleClick(data);
-                                        }}
-                                    >
-                                        <Image
-                                            src={'/icones/icone' + imageInfo + ".png"}
-                                            alt={imageInfo}
-                                            height={0}
-                                            width={0}
-                                            sizes="100vw"
-                                            className=" hover:opacity-40 border-solid border-2 mb-1 border-zinc-800 w-full h-34 sm:h-40 md:h-52 lg:h-28 xl:h-24 2xl:h-48"
-                                        />
-                                    </button>
-                                </div>
-                        ))}
-                        
+                                                const data = {
+                                                    nome: props.perfil.nome,
+                                                    nick: props.perfil.nick,
+                                                    data_nasc: date,
+                                                    icon: "icone" + imageInfo + ".png"
+                                                };
+                                                handleClick(data);
+                                            }}
+                                        >
+                                            <Image
+                                                src={'/icones/icone' + imageInfo + ".png"}
+                                                alt={imageInfo}
+                                                height={0}
+                                                width={0}
+                                                sizes="100vw"
+                                                className=" hover:opacity-40 border-solid border-2 mb-1 border-zinc-800 w-auto h-34 sm:h-40 md:h-52 lg:h-28 xl:h-24 "
+                                            />
+                                        </button>
+                                    </div>
+                            ))}
+                            
+                        </div>
+                        <button
+                            className="bg-gray-500 hover:bg-gray-600 text-white font-semibold py-2 px-6"
+                            onClick={() => setShowConfirmationModal(false)}
+                        >
+                            Cancelar
+                        </button>
                     </div>
-                    <button
-                        className="bg-gray-500 hover:bg-gray-600 text-white font-semibold py-2 px-6"
-                        onClick={() => setShowConfirmationModal(false)}
-                    >
-                        Cancelar
-                    </button>
+
                 </div>
             </Modal>
         </div>
