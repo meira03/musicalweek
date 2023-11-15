941e81278494f911b27891ec2cfe45e31c0fa0ff
Jubirao <gabriels1850@gmail.com.br>
2023-11-15 18:12:35 -0300

    Correção de bugs

--- src/app/[lang]/(protected)/artista/sala/[id]/resumo/page.js
+++ src/app/[lang]/(protected)/artista/sala/[id]/resumo/page.js
@@ -8,7 +8,7 @@ export const metadata = {
   title: "Sala do Artista",
 };
 
-export default async function Page({ params: {  id, params: { lang } } }) {
+export default async function Page({ params: { id, lang } }) {
   let dict = await getDictionary(lang);
   dict = dict.artista_sala_resumo;
 
