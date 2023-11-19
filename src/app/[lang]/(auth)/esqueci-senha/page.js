bf8aa87625cf0d29568f617808342ad3803aff77
felipekenzo175 <felipekenzoknz@gmail.com>
2023-11-18 22:53:19 -0300

    correcao esqueci senha

--- src/app/[lang]/(auth)/esqueci-senha/page.js
+++ src/app/[lang]/(auth)/esqueci-senha/page.js
@@ -1,10 +1,57 @@
-import { getDictionary } from "@/utils/dictionaries";
-import { EsqueciSenha } from "@/components/esqueci-senha/esqueci-senha"
+"use client";
 
-export default async function EsqueciSenha({ params: { lang } }) {
-  let dict = await getDictionary(lang);
-  dict = dict.esqueci_senha;
+import { useState } from "react";
+import { enviarEmail } from "@/utils/forms";
+import { useRouter } from "next/navigation";
+import Input from "@/components/form/Input";
 
-  return< EsqueciSenha dict = {dict.esqueci_senha}/>
+export default function EsqueciSenha() {
+  const router = useRouter();
+  const [error, setError] = useState("");
+
+  async function onRecuperate(formData) {
+    if (document.getElementById("emailEsqueciSenha").value === "") {
+      setError("Campo Obrigatório");
+      document.getElementById("emailEsqueciSenha").classList.add("border-red-500");
+    } else {
+      const res = await enviarEmail(formData);
+
+      if (res.sucesso) {
+        router.push("/esqueci-senha/espera-email/");
+      } else {
+        setError(res.descricao);
+      }
+    }
+  }
+
+  return (
+    <main className="mx-auto sm:max-w-7xl px-2 sm:px-6 lg:px-8 min-h-[80vh] flex justify-center items-center">
+      <div className="font-tech">
+        <h1 className="text-center text-4xl font-bold mb-6 uppercase text-neon-blue-100  neon-text">
+          ESQUECI SENHA
+        </h1>
+        <form action={onRecuperate}>
+          <div className="mb-4">
+            <Input
+              id="emailEsqueciSenha"
+              type="email"
+              name="emailEsqueciSenha"
+              placeholder="DIGITE SEU E-MAIL"
+            />
+            <p id="emailEsqueciSenha-error" className="text-red-500 text-xs italic">
+              {error}
+            </p>
+          </div>
+          <div className="flex items-center justify-center">
+            <button
+              type="submit"
+              className="bg-neon-blue-200 hover:bg-neon-blue-300 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full mb-4"
+            >
+              RECUPERAR SENHA
+            </button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
 }
-
